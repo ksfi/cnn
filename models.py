4369ae3096450f999a59fde69a5b0eddcdb7f37f
Kérian Yousfi <kerian.yousfi@hotmail.fr>
2023-03-06 16:08:49 +0100

    size convnet

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import torch
+import math
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
@@ -8,6 +9,7 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 from tqdm import tqdm
+from torchsummary import summary
 # from pytorch_lightning.callbacks import TQDMProgressBar
 
 OUTPUT_SIZE = 10
@@ -54,14 +56,14 @@ class AlexNet(nn.Module):
     def __init__(self, n_out):
         super(AlexNet, self).__init__()
         self.feature_extract = nn.Sequential(
-                nn.Conv2d(3, 96, 11, stride=4), nn.ReLu(),
-                nn.MaxPool(3, stride=2),
-                nn.Conv2d(96, 256, 5, pad=2), nn.ReLu(),
-                nn.MaxPool(3, stride=2),
-                nn.Conv2d(256, 384, pad=1), nn.ReLu(),
-                nn.Conv2d(384, 384, pad=1), nn.ReLu(),
-                nn.Conv2d(256, 384, pad=1), nn.ReLu(),
-                nn.MaxPool(3, stride=2))
+                nn.Conv2d(3, 96, 11, stride=4), nn.ReLU(),
+                nn.MaxPool2d(3, stride=2),
+                nn.Conv2d(96, 256, 5, pad=2), nn.ReLU(),
+                nn.MaxPool2d(3, stride=2),
+                nn.Conv2d(256, 384, pad=1), nn.ReLU(),
+                nn.Conv2d(384, 384, pad=1), nn.ReLU(),
+                nn.Conv2d(256, 384, pad=1), nn.ReLU(),
+                nn.MaxPool2s(3, stride=2))
         self.Classifier = nn.Sequential(
                 nn.Linear(5*5*256, 4096), nn.Relu(), nn.Dropout(0.5),
                 nn.Linear(4096, 1000), nn.Relu(), nn.Dropout(0.5),
@@ -77,9 +79,31 @@ class ConvNetA(nn.Module):
     def __init__(self, n_out):
         super(ConvNetA, self).__init__()
         self.feature_extract = nn.Sequential(
-                nn.Conv2d(3, 64), nn.ReLu(),
-                nn.Conv2d(64, 128), nn.ReLu()
+                nn.Conv2d(3, 64, 3), nn.ReLU(),
+                nn.MaxPool2d(2, stride=2),
+                nn.Conv2d(64, 128, 3), nn.ReLU(),
+                nn.MaxPool2d(2, stride=2),
+                nn.Conv2d(128, 256, 3), nn.ReLU(),
+                nn.Conv2d(256, 256, 3), nn.ReLU(),
+                nn.MaxPool2d(2, stride=2),
+                nn.Conv2d(256, 512, 3), nn.ReLU(),
+                nn.Conv2d(512, 512, 3), nn.ReLU(),
+                nn.MaxPool2d(2, stride=2),
+                nn.Conv2d(256, 512, 3), nn.ReLU(),
+                nn.Conv2d(512, 512, 3), nn.ReLU(),
+                nn.MaxPool2d(2, stride=2)
                 )
+        self.Classifier = nn.Sequential(
+                nn.Linear(self.feature_extract[20].shape[1]*self.feature_extract[20].shape[2]*self.feature_extract[20].shape[3], 4096), nn.Relu(),
+                nn.Linear(4096, 4096), nn.Relu(),
+                nn.Linear(4096, 1000), nn.Relu()
+                )
+    def forward(self, x):
+        x = nn.feature_extract(x),
+        x = torch.flatten(x, 1),
+        x = self.classifier(x)
+        x = F.softmax(x)
+        return x
 
 
 transforms = transforms.Compose([transforms.Resize((32, 32)),
@@ -102,55 +126,76 @@ valid_loader = DataLoader(dataset=valid_dataset,
                           batch_size=BATCH_SIZE, 
                           shuffle=False)
 
+class Size(nn.Module):
+    def __init__(self, n_out):
+        super(Size, self).__init__()
+        self.feature_extract = nn.Sequential(
+                nn.Conv2d(3, 64, 3, padding=1), nn.ReLU(),
+                nn.MaxPool2d(2, stride=2),
+                nn.Conv2d(64, 128, 3, padding=1), nn.ReLU(),
+                nn.MaxPool2d(2, stride=2),
+                nn.Conv2d(128, 256, 3, padding=1), nn.ReLU(),
+                nn.Conv2d(256, 256, 3, padding=1), nn.ReLU(),
+                nn.MaxPool2d(2, stride=2),
+                nn.Conv2d(256, 512, 3, padding=1), nn.ReLU(),
+                nn.Conv2d(512, 512, 3, padding=1), nn.ReLU(),
+                nn.MaxPool2d(2, stride=2),
+                nn.Conv2d(256, 512, 3, padding=1), nn.ReLU(),
+                nn.Conv2d(512, 512, 3, padding=1), nn.ReLU(),
+                nn.MaxPool2d(2, stride=2)
+                )
+    def forward(self, x):
+        x = self.feature_extract(x)
+        return x
+
+def H_out(H_in, pad, ker, stride):
+    return math.floor(((H_in+2*pad-(ker-1)-1)/stride)+1)
+
+def W_out(W_in, pad, ker, stride):
+    return math.floor(((W_in+2*pad-(ker-1)-1)/stride)+1)
+
 
 if __name__ == "__main__":
-    m = LeNet(10).to(DEVICE)
-    optimizer = optim.Adam(m.parameters(), lr=LEARNING_RATE)
-    criterion = nn.CrossEntropyLoss()
+#     m = LeNet(10).to(DEVICE)
+#     optimizer = optim.Adam(m.parameters(), lr=LEARNING_RATE)
+#     criterion = nn.CrossEntropyLoss()
+# 
+#     loss_fn = nn.CrossEntropyLoss()
+#     optimizer = optim.SGD(m.parameters(), lr=0.1)
+#     m.train()
+#     for epoch in range(n_epochs):
+#         for X_batch, y_batch in tqdm(train_loader):
+#             X_batch, y_batch = X_batch.to(DEVICE), y_batch.to(DEVICE)
+#             y_pred = m(X_batch)
+#             loss = loss_fn(y_pred, y_batch)
+#             optimizer.zero_grad()
+#             loss.backward()
+#             optimizer.step()
+# 
+#     m.eval()
+#     eval_losses=[]
+#     eval_accu=[]
+#     running_loss=0
+#     correct=0
+#     total=0
+#     with torch.no_grad():
+#         for X_batch, y_batch in tqdm(valid_loader):
+#             X_batch, y_batch = X_batch.to(DEVICE), y_batch.to(DEVICE)
+#             y_pred = m(X_batch)
+#             loss = loss_fn(y_pred, y_batch)
+#             running_loss += loss.item()
+#             _, predicted = y_pred.max(1)
+#             total += y_batch.size(0)
+#             correct += y_pred.eq(y_batch.resize_(y_pred.size())).sum().item()
+# 
+#     test_loss=running_loss/len(valid_loader)
+#     accu=100.*correct/total
+#     print(f"correct {correct}, total {total}")
+# 
+#     eval_losses.append(test_loss)
+#     eval_accu.append(accu)
+# 
+#     print('Test Loss: %.3f | Accuracy: %.3f'%(test_loss,accu)) 
 
-    loss_fn = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(m.parameters(), lr=0.1)
-    m.train()
-    for epoch in range(n_epochs):
-        for X_batch, y_batch in tqdm(train_loader):
-            X_batch, y_batch = X_batch.to(DEVICE), y_batch.to(DEVICE)
-            y_pred = m(X_batch)
-            loss = loss_fn(y_pred, y_batch)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-    m.eval()
-    eval_losses=[]
-    eval_accu=[]
-    running_loss=0
-    correct=0
-    total=0
-    with torch.no_grad():
-        for X_batch, y_batch in tqdm(valid_loader):
-            X_batch, y_batch = X_batch.to(DEVICE), y_batch.to(DEVICE)
-            y_pred = m(X_batch)
-            loss = loss_fn(y_pred, y_batch)
-            running_loss += loss.item()
-            _, predicted = y_pred.max(1)
-            total += y_batch.size(0)
-            correct += y_pred.eq(y_batch.resize_(y_pred.size())).sum().item()
-
-    test_loss=running_loss/len(valid_loader)
-    accu=100.*correct/total
-    print(f"correct {correct}, total {total}")
-
-    eval_losses.append(test_loss)
-    eval_accu.append(accu)
-
-    print('Test Loss: %.3f | Accuracy: %.3f'%(test_loss,accu)) 
-
-
-
-
-
-
-
-
-
-
+    s = Size(1000)
+    summary(s, (3, 224, 224))
